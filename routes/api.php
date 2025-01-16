40cfd10f52e0706c7b6ffe14356ad1d160f71319
sutodiryo <hubungi.yogi@gmail.com>
2025-01-16 12:19:36 +0700

    Fix Route Api

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\ProductController;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware(Authenticate::using('sanctum'));
+
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', [LoginController::class, 'login'])->name('login');
+});
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    // Post
+    Route::apiResource('/post', App\Http\Controllers\Api\PostController::class);
+
+    Route::prefix('post')->as('post.')->group(function () {
+        Route::post('/tags/{id}', [PostController::class, 'tags'])->name('tags');
+        Route::post('/images/{id}', [PostController::class, 'images'])->name('images');
+    });
+
+    // Product
+    Route::apiResource('/product', App\Http\Controllers\Api\ProductController::class);
+
+    Route::prefix('product')->as('product.')->group(function () {
+        Route::post('/tags/{id}', [ProductController::class, 'tags'])->name('tags');
+        Route::post('/images/{id}', [ProductController::class, 'images'])->name('images');
+    });
+});
